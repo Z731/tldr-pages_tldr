60180583d169374ca89ba06c6b796888fc9b1fa6
Zhongwei JIANG <zhongwei.jiang@nio.com>
2023-01-18 17:14:38 +0800

    update z731.sh

--- z731.sh
+++ z731.sh
@@ -5,20 +5,20 @@ ls | grep "pages" | grep -v "pages.zh" | grep "pages." | xargs -I {} rm -r {}
 
 # mkdir z731
 if [ ! -d "z731" ]; then mkdir z731; fi
-# ls pages/* | sed 's/.md$//g' | head
-# ls pages/* | sed 's/.md$//g' | tail
+# ls pages/* | sed 's/\.md$//' | head
+# ls pages/* | sed 's/\.md$//' | tail
 # brew install tree
-# tree pages | sed 's/.md$//g' | head
-# tree pages | sed 's/.md$//g' | tail
-# tree pages | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > tmp0
-# tree pages | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > tmp
-tree pages | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages
-tree pages.zh | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages.zh
-tree pages.zh_TW | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages.zh_TW
+# tree pages | sed 's/\.md$//' | head
+# tree pages | sed 's/\.md$//' | tail
+# tree pages | sed 's/\.md$//;s/├── /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > tmp0
+# tree pages | sed 's/\.md$//;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > tmp
+tree pages | sed 's/\.md$//;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages
+tree pages.zh | sed 's/\.md$//;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages.zh
+tree pages.zh_TW | sed 's/\.md$//;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages.zh_TW
 cp z731/tree_pages z731/read_tree_pages
 cp z731/tree_pages.zh z731/read_tree_pages.zh
 cp z731/tree_pages.zh_TW z731/read_tree_pages.zh_TW
-tree -i -f pages* | sed 's/.md$//g' > z731/tree_pages_all
+tree -i -f pages* | sed 's/\.md$//' > z731/tree_pages_all
 awk -F '/' '{for(i=0;i<=NF-1;i++)printf("%s\t",$(NF-i));printf("\n");}' z731/tree_pages_all | sort > z731/tree_pages_all_revert
 cp z731/tree_pages_all_revert z731/read_tree_pages_all_revert
 
@@ -33,6 +33,6 @@ grep "github.io" z731/grep_pages_all_http_more_info_url_github > z731/grep_pages
 grep -v "github.com" z731/grep_pages_all_http_more_info_url_github | grep -v "github.io" > z731/grep_pages_all_http_more_info_url_github_other
 wc -l z731/grep_pages_all_http_more_info_url_github_*
 
-awk -F '[/:]' '{for(i=0;i<=3-1;i++)printf("%s\t",$(3-i));for(i=4;i<=NF;i++)printf("%s/",$(i));printf("\n");}' z731/grep_pages_all_http_more_info | sed 's/\/\/\//:\/\//g' | sort > z731/grep_pages_all_http_more_info_revert
+awk -F '[/:]' '{for(i=0;i<=3-1;i++)printf("%s\t",$(3-i));for(i=4;i<=NF;i++)printf("%s/",$(i));printf("\n");}' z731/grep_pages_all_http_more_info | sed 's/\/\/\//:\/\//g;s/\/$//g;s/\.md//' | sort > z731/grep_pages_all_http_more_info_revert
 sort -t '<' -k 2 z731/grep_pages_all_http_more_info_revert > z731/grep_pages_all_http_more_info_revert_sort_url
 
