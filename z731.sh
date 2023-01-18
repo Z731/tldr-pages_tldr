91606313459c3a1c20d161e0b5b68e9888ecf698
Zhongwei JIANG <zhongwei.jiang@nio.com>
2023-01-18 08:04:57 +0800

    update z731.sh

--- z731.sh
+++ z731.sh
@@ -2,3 +2,16 @@
 # touch z731.sh
 ls | grep "pages" | grep -v "pages.zh" | grep "pages." | xargs -I {} echo {} | xargs
 ls | grep "pages" | grep -v "pages.zh" | grep "pages." | xargs -I {} rm -r {}
+
+# mkdir z731
+if [ ! -d "z731" ]; then mkdir z731; fi
+# ls pages/* | sed 's/.md$//g' | head
+# ls pages/* | sed 's/.md$//g' | tail
+# brew install tree
+# tree pages | sed 's/.md$//g' | head
+# tree pages | sed 's/.md$//g' | tail
+# tree pages | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > tmp0
+# tree pages | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > tmp
+tree pages | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages
+tree pages.zh | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages.zh
+tree pages.zh_TW | sed 's/.md$//g;s/├── /\t/g;s/│   /\t/g;s/│   /\t/g;s/└── /\t/g;s/    /\t/g' > z731/tree_pages.zh_TW
