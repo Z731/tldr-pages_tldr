50e67803eb2663440f91edc6ebb565f64c715248
Z731 <z731@live.com>
2023-01-20 12:50:02 +0800

    update z731.sh

--- z731.sh
+++ z731.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# chmod +x *.sh
 # touch z731.sh
 ls | grep "pages" | grep -v "pages.zh" | grep "pages." | xargs -I {} echo {} | xargs
 ls | grep "pages" | grep -v "pages.zh" | grep "pages." | xargs -I {} rm -r {}
